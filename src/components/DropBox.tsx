d35e0557de85221dab3da4eddec5b75162051c80
choyeon <cho1428hee@naver.com>
2025-06-25 11:49:36 +0900

    feat: DropBox 구성

--- src/components/DropBox.tsx
+++ src/components/DropBox.tsx
@@ -1,15 +1,54 @@
-import type { IName } from '../types/name';
+import { useState } from "react";
+import type { IName } from "../types/name";
+import styled from "styled-components";
 
 interface DropBox {
   names: IName[];
 }
 
 const DropBox = ({ names }: DropBox) => {
-  /**
-   * TODO : implement the DropBox component
-   */
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedName, setSelectedName] = useState<IName | null>(null);
 
-  return <div>Drop Box</div>;
+  const toggleOpen = () => setIsOpen((prev) => !prev);
+
+  return (
+    <Container>
+      <DropBoxButton onClick={toggleOpen}>
+        {selectedName?.name || "김초연"}
+      </DropBoxButton>
+      {isOpen && <Options></Options>}
+    </Container>
+  );
 };
 
 export default DropBox;
+
+const Container = styled.div`
+  width: 100px;
+`;
+
+const DropBoxButton = styled.button`
+  width: 100%;
+  padding: 6px 10px;
+  background: white;
+  border: 1px solid #000;
+  border-radius: 4px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Options = styled.ul`
+  background: white;
+  border-radius: 4px;
+  border: none;
+  display: flex;
+  padding: 8px;
+  margin: 2px 0;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 12px;
+  align-self: stretch;
+`;
