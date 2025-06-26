52410d3604f0198f469b318f034c4292e1ea2424
JiiminHa <tracygkwlals@sookmyung.ac.kr>
2025-06-26 15:59:58 +0900

    chore: tailwindcss 초기 설정 및 커스텀 컬러 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          350: "#CECECE",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
