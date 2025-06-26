0126057e3cd030740f322d110bb0928fd83fc407
ion <delion.dev@gmail.com>
2025-06-27 02:41:56 +0900

    feat: DropBoxSelect 클릭 시 DropBoxOptions 표시 여부 제어 기능 구현
    DropBoxSelect 컴포넌트를 클릭하면
    DropBoxOptions의 표시 여부를 토글하는 로직을 구현함.

--- src/components/DropBox.tsx
+++ src/components/DropBox.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { IName } from "../types/name";
 import DropBoxOptions from "./DropBoxOptions";
 import DropBoxSelect from "./DropBoxSelect";
@@ -7,10 +8,12 @@ interface DropBox {
 }
 
 const DropBox = ({ names }: DropBox) => {
+  const [show, setShow] = useState(false);
+
   return (
     <div className="flex w-100 flex-col items-start gap-2">
-      <DropBoxSelect name={"조원영"} />
-      <DropBoxOptions names={names} />
+      <DropBoxSelect name={"조원영"} show={show} setShow={setShow} />
+      {show && <DropBoxOptions names={names} />}
     </div>
   );
 };
--- src/components/DropBoxSelect.tsx
+++ src/components/DropBoxSelect.tsx
@@ -3,11 +3,20 @@ import ChevronUp from "../assets/chevron-up.svg?react";
 
 interface DropBoxSelectProps {
   name: string;
+  show: boolean;
+  setShow: (b: boolean) => void;
 }
 
-const DropBoxSelect: React.FC<DropBoxSelectProps> = ({ name }) => {
+const DropBoxSelect: React.FC<DropBoxSelectProps> = ({
+  name,
+  show,
+  setShow,
+}) => {
   return (
-    <div className="flex p-[6px_10px] items-center self-stretch rounded-[4px] border-1 border-solid border-black bg-white">
+    <div
+      onClick={() => setShow(!show)}
+      className="flex p-[6px_10px] items-center self-stretch rounded-[4px] border-1 border-solid border-black bg-white"
+    >
       <div className="w-56">{name}</div>
       <ChevronUp className="w-24 h-24 aspect-square" />
     </div>
