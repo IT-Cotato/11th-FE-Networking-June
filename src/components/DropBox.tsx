a2d1b85b93f0ea60a06208074c0ab2b2f9f010a7
JiiminHa <tracygkwlals@sookmyung.ac.kr>
2025-06-26 14:13:28 +0900

    feat: DropBox 기본 상태 및 토글 기능 구현

--- src/components/DropBox.tsx
+++ src/components/DropBox.tsx
@@ -8,7 +8,31 @@ const DropBox = ({ names }: DropBox) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selected, setSelected] = useState<IName | null>(null);
 
-  return <div>Drop Box</div>;
+  const handleToggle = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleSelect = (item: IName) => {
+    setSelected(item);
+    setIsOpen(false);
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={handleToggle}>
+          <span>{selected ? selected.name : "이름을 입력하세요"}</span>
+          <img src="/src/assets/chevron-up.svg" alt="토글 아이콘" />
+        </button>
+      </div>
+      <div>
+        {names.map((item) => (
+          <button key={item.id} onClick={() => handleSelect(item)}>
+            {item.name}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default DropBox;
