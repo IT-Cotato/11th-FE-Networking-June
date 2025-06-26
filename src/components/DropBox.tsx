e7ebf8c9a98d6f396800d1bdbdfac106bcae188c
ion <delion.dev@gmail.com>
2025-06-27 02:12:40 +0900

    feat: DropBoxOptions 컴포넌트 구현
    DropBox 컴포넌트 내부에 DropBoxSelect와 DropBoxOptions를 포함시켜
    드롭다운 UI를 하나의 컴포넌트로 통합 구성함.

--- src/components/DropBox.tsx
+++ src/components/DropBox.tsx
@@ -1,15 +1,18 @@
-import type { IName } from '../types/name';
+import type { IName } from "../types/name";
+import DropBoxOptions from "./DropBoxOptions";
+import DropBoxSelect from "./DropBoxSelect";
 
 interface DropBox {
   names: IName[];
 }
 
 const DropBox = ({ names }: DropBox) => {
-  /**
-   * TODO : implement the DropBox component
-   */
-
-  return <div>Drop Box</div>;
+  return (
+    <div className="flex w-100 flex-col items-start gap-2">
+      <DropBoxSelect name={"조원영"} />
+      <DropBoxOptions names={names} />
+    </div>
+  );
 };
 
 export default DropBox;
