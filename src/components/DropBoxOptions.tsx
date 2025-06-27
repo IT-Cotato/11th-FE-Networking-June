f0c09508a3a997c98ba0bccd17a171b88792ed58
ion <delion.dev@gmail.com>
2025-06-27 11:50:54 +0900

    refactor: 불필요한 코드 삭제

--- src/components/DropBoxOptions.tsx
+++ src/components/DropBoxOptions.tsx
@@ -4,22 +4,17 @@ import type { IName } from "../types/name";
 
 interface DropBoxOptionsProps {
   nameArray: IName[];
-  ref?: React.Ref<HTMLDivElement>;
   setSelected: (v: number) => void;
   setShow: (b: boolean) => void;
 }
 
 const DropBoxOptions: React.FC<DropBoxOptionsProps> = ({
   nameArray,
-  ref,
   setSelected,
   setShow,
 }) => {
   return (
-    <div
-      ref={ref}
-      className="flex p-8 flex-col items-start gap-12 self-stretch rounded-[4px] bg-white"
-    >
+    <div className="flex p-8 flex-col items-start gap-12 self-stretch rounded-[4px] bg-white">
       {nameArray.map((v) => (
         <ItemBox
           key={v.id}
