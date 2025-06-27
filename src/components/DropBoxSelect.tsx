d7b74cc266858da1e11739b09191679194624894
ion <delion.dev@gmail.com>
2025-06-27 10:46:13 +0900

    feat: hover 시 배경색 변경 및 transition 효과 적용
    ItemBox와 DropBoxSelect 컴포넌트에 hover 시 배경색이 변경되도록 설정하고, transition 효과를 추가하여 색상 전환이 부드럽게 이루어지도록 구현함.

--- src/components/DropBoxSelect.tsx
+++ src/components/DropBoxSelect.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ChevronUp from "../assets/chevron-up.svg?react";
+import { ANIMATION_DURATION_BG } from "../constants/values";
 
 interface DropBoxSelectProps {
   name: string;
@@ -15,7 +16,8 @@ const DropBoxSelect: React.FC<DropBoxSelectProps> = ({
   return (
     <div
       onClick={() => setShow(!show)}
-      className="flex p-[6px_10px] items-center self-stretch rounded-[4px] border-1 border-solid border-black bg-white"
+      className={`flex p-[6px_10px] items-center self-stretch rounded-[4px] border-1 border-solid border-black bg-white
+      hover:bg-blue-100 transition-bg duration-${ANIMATION_DURATION_BG} ease-out`}
     >
       <div className="w-56">{name}</div>
       <ChevronUp
--- src/components/ItemBox.tsx
+++ src/components/ItemBox.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ANIMATION_DURATION_BG } from "../constants/values";
 
 interface ItemBoxProps {
   name: string;
@@ -9,7 +10,7 @@ interface ItemBoxProps {
 const ItemBox: React.FC<ItemBoxProps> = ({ name, setSelected, setShow }) => {
   return (
     <div
-      className="self-stretch"
+      className={`self-stretch hover:bg-blue-100 transition-bg duration-${ANIMATION_DURATION_BG} ease-out`}
       onClick={() => {
         setSelected(name);
         setShow(false);
